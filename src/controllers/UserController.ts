f2446368fe7df331cac398e099ce12fee7e0d580
João Vardenski <joaovvandrade@gmail.com>
2026-01-13 16:24:41 -0300

    Changing cookies configuration for cors

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -31,8 +31,8 @@ export class UserController {
 
       res.cookie("access_token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: true,
+        sameSite: "none",
         maxAge: 1000 * 60 * 60 * 24 * 30,
       });
 
